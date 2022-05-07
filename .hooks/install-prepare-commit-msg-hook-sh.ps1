c98027436bd8fedfd387741dd84479b10eee8b96
Artur Mustafin <artur.mustafin@wickes.co.uk>
2022-05-07 08:32:12 +0300

    [main] added bash version

--- .hooks/install-prepare-commit-msg-hook-sh.ps1
+++ .hooks/install-prepare-commit-msg-hook-sh.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 #!C:/Program\ Files/PowerShell/7/pwsh.exe
-& Copy-Item $PSScriptRoot/prepare-commit-msg $PSScriptRoot/../.git/hooks/prepare-commit-msg
+& Copy-Item $PSScriptRoot/prepare-commit-msg-sh $PSScriptRoot/../.git/hooks/prepare-commit-msg
 if ($IsLinux) {
     chmod u+x $PSScriptRoot/../.git/hooks/prepare-commit-msg
 }
